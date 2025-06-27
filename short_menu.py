c532bb4c480f8075b1e778c0a34fb09f8011acc4
pepe <p.zajonz@posteo.de>
2025-06-27 10:43:38 +0200

    restoring of former state of ascii.py

--- short_menu.py
+++ short_menu.py
@@ -1,10 +1,14 @@
 def short_menu():
-	print("Do you want to play again?")
-	answer = ""
-	while answer != 'yes' or answer != 'no':
-          answer = input("Enter 'yes' for replay, enter 'no' to quit")
-          if answer == "yes":
-              return True
-          elif answer == "no":
-              return False
+    """
+    Short menu for to choose if someone want to play again.
+    returns Boolean
+    """
+    print("Do you want to play again?")
+    answer = ""
+    while answer != 'yes' or answer != 'no':
+            answer = input("Enter 'yes' for replay, enter 'no' to quit ")
+            if answer == "yes":
+                return True
+            elif answer == "no":
+                return False
       
